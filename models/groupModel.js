250cb182b1a47f08a00d45cf2cb240ce02f5f1cf
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-02-26 12:08:33 +0200

    creating new models

---
+++ models/groupModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const User = require('./userModel');
+
+const groupSchema = new mongoose.Schema({
+    groupName : {
+        type: String,
+        required: [true , 'group must have an name'],
+        unique: [true],
+        lowercase: [true],
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    adminUser :{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    
+    },
+
+    users :[{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    }],
+
+    active: {
+        type :Boolean,
+        default: true,
+        select: false
+    }
+});
+
+
+
+const Group = mongoose.model('Group' , groupSchema);
+module.exports = Group; 
