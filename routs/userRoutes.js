7486d5da6a2fc1f815505bfb5069b58fe535f08e
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-03-03 22:07:09 +0200

    finish user routes

--- routs/userRoutes.js
+++ routs/userRoutes.js
@@ -4,8 +4,6 @@ const authController = require('../controllers/authController.js');
 
 const router = express.Router();
 
-
-
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
@@ -15,21 +13,21 @@ router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 
-router.get('/me' , userController.getMe , userController.getUser);
+router.get('/me', userController.getMe, userController.getUser);
 router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
 
 router
-    .route('/')
-    .get(userController.getAllUsers)
-    .post(userController.createUser);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router
-    .route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser);
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
