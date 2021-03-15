a8fe65533cc0ab57e66818c0f09f1b991b8ef020
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-03-15 12:52:49 +0200

    replacing algo

--- runBaseballAlgo.js
+++ runBaseballAlgo.js
@@ -1,6 +1,6 @@
 // var child = require('child_process').spawn('java' ,['-jar', `${__dirname}\\test.jar`,`${__dirname}\\teams24.txt`]);
 var pro = require('child_process');
-const child = pro.spawn(`${__dirname}\\cppSRC\\baseballElimination.exe`, [
+const child = pro.spawn(`${__dirname}\\cppSRC\\BasketballEliminationProblem.exe`, [
   `${__dirname}\\cppSRC\\input1.txt`,
 ]);
 
