c1118176560fa01b3a2751978daaa1755c2b7d70
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-03-15 14:27:00 +0200

    finish route

--- routs/algoRoutes.js
+++ routs/algoRoutes.js
@@ -4,6 +4,6 @@ const algoController = require('../controllers/algoController');
 const router = express.Router();
 
 router.route('/').get(algoController.checkChances);
-router.route('/makealgofile').get(algoController.algoSetup);
+router.route('/standings').get(algoController.algoSetup);
 
 module.exports = router;
