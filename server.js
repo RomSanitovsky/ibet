1c8c0f732aeda3c1a71d8a80d920f5ae4210d64f
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-03-30 12:54:12 +0300

    check

--- server.js
+++ server.js
@@ -1,43 +1,45 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-process.on('uncaughtException', err => {
-    console.log('UNHANDLER EXEPTION!    SHUTING DOWN...');
-    console.log(err.name , err.message , err.stack);
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLER EXEPTION!    SHUTING DOWN...');
+  console.log(err.name, err.message, err.stack);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    autoIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.log(err.name, err.message);
+  });
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`app running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLER REJECTION!    SHUTING DOWN...');
+  console.log(err.name, err.message, err.stack);
+  server.close(() => {
     process.exit(1);
+  });
+});
 
-})
-
-dotenv.config({path: './config.env'});
-
-const DB= process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD);
-mongoose.connect(DB ,
-    {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        autoIndex: true,
-        useFindAndModify: false
-    })
-    .then(()=>{console.log("DB connection successful!");
-    })
-    .catch(err => {
-        console.log(err.name , err.message);
-    });
-
-
-    const app = require('./app');
-
-    const port=process.env.PORT || 3000;
-    const server = app.listen(port , ()=>{
-        console.log(`app running on port ${port}...`);
-    });
-    
-    
-    process.on('unhandledRejection', err => {
-        console.log('UNHANDLER REJECTION!    SHUTING DOWN...');
-        console.log(err.name , err.message , err.stack);
-        server.close(() => {
-            process.exit(1);
-        });
-    });
-    
+//
