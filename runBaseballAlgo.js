74cc0f34fe888679d1613550a599a17c95896ef7
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-03-30 21:46:27 +0300

    make it better

--- runBaseballAlgo.js
+++ runBaseballAlgo.js
@@ -1,7 +1,7 @@
 // var child = require('child_process').spawn('java' ,['-jar', `${__dirname}\\test.jar`,`${__dirname}\\teams24.txt`]);
 var pro = require('child_process');
-const child = pro.spawn(`${__dirname}\\cppSRC\\BasketballEliminationProblem.exe`, [
-  `${__dirname}\\cppSRC\\input1.txt`,
+const child = pro.spawn(`.\\..\\cppSRC\\BasketballEliminationProblem.exe`, [
+  `.\\..\\cppSRC\\input1.txt`,
 ]);
 
 child.on('close', (code) => {
