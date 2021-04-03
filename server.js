93af6b126154f32f588b50d8fbabab6b023f21d4
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-04-04 02:01:20 +0300

    1

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const axios = require('axios');
+const fs = require('fs');
+const League = require('../models/leagueModel');
+const Team = require('../models/teamModel');
 
 process.on('uncaughtException', (err) => {
   console.log('UNHANDLER EXEPTION!    SHUTING DOWN...');
@@ -32,6 +36,128 @@ mongoose
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+
+const dataMaker = async () => {
+  await axios
+    .get('https://api-nba-v1.p.rapidapi.com/standings/standard/2020-2021', {
+      headers: {
+        'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'f97df4d38dmsh6bc2a968b7e539bp17dcb1jsn9c38f4b94a67',
+      },
+    })
+    .then((data) =>
+      fs.writeFileSync(
+        './getExampleStandings.json',
+        JSON.stringify(data.data.api)
+      )
+    );
+  console.log('check1');
+
+  await axios
+    .get('https://api-nba-v1.p.rapidapi.com/games/seasonYear/2020-2021', {
+      headers: {
+        'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'f97df4d38dmsh6bc2a968b7e539bp17dcb1jsn9c38f4b94a67',
+        useQueryString: true,
+      },
+    })
+    .then((data) =>
+      fs.writeFileSync('./getExampleGames.json', JSON.stringify(data.data.api))
+    );
+
+  await axios
+    .get('https://api-nba-v1.p.rapidapi.com/teams/league/standard', {
+      headers: {
+        'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'f97df4d38dmsh6bc2a968b7e539bp17dcb1jsn9c38f4b94a67',
+        useQueryString: true,
+      },
+    })
+    .then((data) =>
+      fs.writeFileSync('./getTeams.json', JSON.stringify(data.data.api))
+    );
+
+  const standings = JSON.parse(fs.readFileSync('./getExampleStandings.json'));
+  const games = JSON.parse(fs.readFileSync('./getExampleGames.json'));
+  const teams = JSON.parse(fs.readFileSync('./getTeams.json')).teams;
+
+  const teamMap = new Map();
+
+  try {
+    await League.deleteMany();
+    await Team.deleteMany();
+  } catch (err) {
+    console.log(err);
+  }
+
+  const results = [];
+  var temp;
+  for (let i = 0; i < 30; i++) {
+    results[i] = {};
+    results[i].id = (i + 1).toString();
+    results[i].apiId = standings.standings[i].teamId;
+
+    //Set up map between 1-30 ids to API ids
+    teamMap[standings.standings[i].teamId] = i + 1;
+
+    teams.forEach((team) => {
+      if (team.teamId === results[i].apiId) {
+        results[i].teamName = team.fullName;
+      }
+    });
+    results[i].wins = standings.standings[i].win;
+    results[i].losses = standings.standings[i].loss;
+    results[i].remaning = 72 - results[i].wins - results[i].losses;
+    Team.create(results[i]);
+    for (let j = 0; j <= 30; j++) {
+      results[i][j] = 0;
+    }
+  }
+  //getting remaining games
+  for (let i = 0; i < 30; i++) {
+    games.games.forEach((element) => {
+      if (element.statusGame != 'Finished') {
+        if (element.vTeam.teamId == results[i].apiId) {
+          let opponent = teamMap[element.hTeam.teamId];
+          results[i][opponent.toString()] = results[i][opponent.toString()] + 1;
+        }
+        if (element.hTeam.teamId == results[i].apiId) {
+          let opponent = teamMap[element.vTeam.teamId];
+          results[i][opponent.toString()] = results[i][opponent.toString()] + 1;
+        }
+      }
+    });
+  }
+
+  console.log('writing');
+  fs.writeFileSync('./input.txt', `${results.length}`);
+  fs.appendFileSync('./input.txt', '\n');
+  console.log('writing');
+  results.forEach((el) => {
+    fs.appendFileSync('./input.txt', `${el.teamName.split(' ').join('_')} `);
+    fs.appendFileSync('./input.txt', `${el.wins} `);
+    fs.appendFileSync('./input.txt', `${el.losses} `);
+    fs.appendFileSync('./input.txt', `${el.remaning} `);
+    for (let i = 1; i <= 30; i++) {
+      fs.appendFileSync('./input.txt', `${el[i.toString()]} `);
+    }
+    fs.appendFileSync('./input.txt', '\n');
+  });
+
+  var pro = require('child_process');
+  const child = pro.spawn(`${__dirname}/../cppSRC/baseballElimination`, [
+    `./input.txt`,
+  ]);
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  console.log('Done! data is ready now!');
+};
+
+dataMaker();
+
 const server = app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });
