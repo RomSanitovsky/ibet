f7f960cbc5cc29af1789bdbccc2b1c28d6ebd6c4
RomSanitovsky <66727898+RomSanitovsky@users.noreply.github.com>
2021-04-07 19:30:03 +0300

    1

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const Group = require('../models/groupModel');
 
 exports.createGroup = catchAsync(async (req, res, next) => {
-  const [body, user] = { req };
+  const { body, user } = { req };
   body.adminUser = user._id;
   body.users = [user._id];
 
