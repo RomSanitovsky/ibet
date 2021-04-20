8a8d4679eaac63d99be1bbde501f0f555b3fff57
oberger96 <oberger96@gmail.com>
2021-04-20 08:16:04 +0300

    1

--- controllers/upcommingGamesController.js
+++ controllers/upcommingGamesController.js
@@ -7,7 +7,7 @@ const User = require('../models/userModel');
 exports.getUpcommingGames = catchAsync(async (req, res, next) => {
   const result = await upcomingGames.findOne();
   result.games.sort((a, b) => {
-    return new date(a.date) - new date(b.date);
+    return new Date(a.date) - new Date(b.date);
   });
   res.status(200).json({
     status: 'success',
