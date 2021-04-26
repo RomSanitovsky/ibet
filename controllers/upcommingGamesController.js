8038ce5e9a9f3b768db1fc225fbd213ac739c9c4
RomSanitovsky <romrom306@gmail.com>
2021-04-26 18:32:37 -0500

    population

--- controllers/upcommingGamesController.js
+++ controllers/upcommingGamesController.js
@@ -5,7 +5,7 @@ const upcomingGames = require('../models/upcomingGamesModel');
 const User = require('../models/userModel');
 
 exports.getUpcommingGames = catchAsync(async (req, res, next) => {
-  const result = await upcomingGames.findOne(); //TODO : populate teams
+  const result = await upcomingGames.findOne().populate('games.hTeam').populate('games.vTeam'); //TODO : populate teams
   result.games.sort((a, b) => {
     return new Date(a.date) - new Date(b.date);
   });
